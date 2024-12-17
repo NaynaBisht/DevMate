33e1dc4f32390fa7f591b262fd7c8288a7aa94e4
NaynaBisht1 <145502271+NaynaBisht@users.noreply.github.com>
2024-12-17 12:51:27 +0530

    setup

---
+++ backend/index.js
@@ -0,0 +1,39 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+import userRoutes from "./routes/user.route.js";
+dotenv.config({});
+
+const app = express();
+
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+const corsOptions = {
+    origin:'http://localhost:5173', 
+    credentials:true
+}
+
+app.use(cors(corsOptions));
+
+const PORT = process.env.PORT || 3000;
+
+
+// apis
+
+app.use("/api/v1/user", userRoutes);
+
+// "http://localhost:8000/api/v1/user/register"
+// "http://localhost:8000/api/v1/user/login"
+// "http://localhost:8000/api/v1/user/updateProfile"
+
+
+app.listen(PORT, ()=>{
+    connectDB();
+    console.log(`Server running at port ${PORT}`);
+
+})
