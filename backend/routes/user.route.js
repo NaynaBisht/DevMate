1f08b5be1e14f899a47aae95950a6fd2cd976678
NaynaBisht1 <145502271+NaynaBisht@users.noreply.github.com>
2024-12-17 12:51:35 +0530

    routes

---
+++ backend/routes/user.route.js
@@ -0,0 +1,12 @@
+import express from 'express';  
+import { register, updateProfile, login, logout } from '../controller/user.controller.js';
+import isAuthenticated from '../middleware/isAuthenticated.js';
+
+const router = express.Router();
+
+router.route("/register").post(register);
+router.route("/login").post(login);
+router.route("/logout").get(logout);
+router.route("/profile/update").post(isAuthenticated, updateProfile);
+
+export default router;
