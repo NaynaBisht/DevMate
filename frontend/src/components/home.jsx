ca68e00490648d404ffe632ee4fdeb2d91339952
NaynaBisht1 <145502271+NaynaBisht@users.noreply.github.com>
2024-12-22 23:20:47 +0530

    browse page

--- frontend/src/components/home.jsx
+++ frontend/src/components/home.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import Navbar from './shared/navbar.jsx'
+import HeroSection from './HeroSection.jsx'
+import CategoryCarousel from './CategoryCarousel.jsx'
+import LatestJobs from './LatestJobs.jsx'
+import Footer from './shared/Footer.jsx'
+
 
 function home() {
   return (
     <div>
       <Navbar/>
+      <HeroSection/>
+      <CategoryCarousel/>
+      <LatestJobs/>
+      <Footer/>
     </div>
   )
 }
