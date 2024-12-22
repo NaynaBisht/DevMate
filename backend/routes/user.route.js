8401b8ce2bb7f8dccfc8372ad75a393e0016696c
NaynaBisht1 <145502271+NaynaBisht@users.noreply.github.com>
2024-12-22 16:24:58 +0530

    storing files

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,10 +1,11 @@
 import express from 'express';  
 import { register, updateProfile, login, logout } from '../controller/user.controller.js';
 import isAuthenticated from '../middleware/isAuthenticated.js';
+import { singleUpload } from '../middleware/mutler.js';
 
 const router = express.Router();
 
-router.route("/register").post(register);
+router.route("/register").post( singleUpload ,register);
 router.route("/login").post(login);
 router.route("/logout").get(logout);
 router.route("/profile/update").post(isAuthenticated, updateProfile);
