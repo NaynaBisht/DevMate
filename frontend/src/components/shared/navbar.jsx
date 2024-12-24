f0f7d4b7dc301f7c940065759d19bf6850842d56
NaynaBisht1 <145502271+NaynaBisht@users.noreply.github.com>
2024-12-24 14:53:51 +0530

    profile popover

--- frontend/src/components/shared/navbar.jsx
+++ frontend/src/components/shared/navbar.jsx
@@ -4,9 +4,11 @@ import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import {Popover,PopoverContent,PopoverTrigger} from "@/components/ui/popover";
 import { LogOut, User2 } from "lucide-react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function navbar() {
-  const user = false;
+  
+  const {user} = useSelector(store => store.auth);
 
   return (
     <div className="bg-white">
@@ -22,9 +24,9 @@ function navbar() {
 
         <div className="flex items-center gap-9">
           <ul className="flex font-medium items-center gap-5">
-            <li> Home </li>
-            <li> Jobs </li>
-            <li> Browse </li>
+            <li> <Link to="/" >Home</Link> </li>
+            <li> <Link to="/jobs" >Jobs</Link> </li>
+            <li> <Link to="/browse">Browse </Link> </li>
           </ul>
 
           {!user ? (
@@ -61,7 +63,7 @@ function navbar() {
                 <div className="flex flex-col my-2 text-gray-600">
                   <div className="flex w-fit items-center gap-2 cursor-pointer">
                     <User2 />
-                    <Button variant="link">View Profile</Button>
+                    <Button variant="link"> <Link to="/profile">View Profile</Link> </Button>
                   </div>
 
                   <div className="flex w-fit items-center gap-2 cursor-pointer">
