20237f5e79839a2057276a37347a80f1a4bad09b
NaynaBisht1 <145502271+NaynaBisht@users.noreply.github.com>
2024-12-30 01:24:01 +0530

    filter jobs

--- frontend/src/components/LatestJobCards.jsx
+++ frontend/src/components/LatestJobCards.jsx
@@ -1,12 +1,31 @@
 import React from "react";
 import { Badge } from "./ui/badge";
+import { Avatar, AvatarImage, AvatarFallback } from "./ui/avatar";
+import image from "../assets/images/circle-line-simple-design-logo-600nw-2174926871.webp";
 
+import { useNavigate } from 'react-router-dom';
 const LatestJobCards = ({job}) => {
+  const navigate = useNavigate();
   return (
-    <div className="p-5 rounded-md shadow-xl border bg-white border-gray-100 cursor-pointer">
-      <div>
-        <h1 className="font-medium text-lg" > {job?.company?.name} </h1>
-        <p className=" text-sm text-gray-500 ">{job?.location}</p>
+    <div 
+      onClick={() => navigate(`/description/${job._id}`)}
+      className="p-5 rounded-md shadow-xl border bg-white border-gray-100 cursor-pointer">
+      <div className="flex items-center gap-2 my-2">
+        {/* Avatar outside Button */}
+        <Avatar className="w-12 h-12">
+          <AvatarImage
+            src={job?.company?.logo || image}
+            alt="Company Logo"
+            className="w-full h-full object-contain"
+          />
+          <AvatarFallback className="bg-gray-300 text-gray-500">
+            CN
+          </AvatarFallback>
+        </Avatar>
+        <div>
+          <h1 className="font-bold">{ job?.company?.name }</h1>
+          <p>{ job?.location }</p>
+        </div>
       </div>
       <div>
         <h1 className="font-bold text-lg my-2"> {job?.title } </h1>
